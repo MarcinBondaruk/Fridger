9a1d9ebf3ef5cc3fdcd703bc751cb05ac2b114e1
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2022-01-21 18:01:19 +0100

    fixed permissions

--- docker/s6-overlay/local/etc/cont-init.d/01-composer-install.sh
+++ docker/s6-overlay/local/etc/cont-init.d/01-composer-install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-RUN php /var/www/app/composer.phar install --working-dir=/var/www/app --no-dev --prefer-dist --no-scripts --no-progress --optimize-autoloader
+php /var/www/app/composer.phar install --working-dir=/var/www/app --prefer-dist --no-scripts --no-progress --optimize-autoloader
