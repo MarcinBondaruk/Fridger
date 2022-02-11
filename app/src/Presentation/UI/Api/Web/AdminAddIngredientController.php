79c89f0e9b5b9888ba4fe988dddfec9283407d9e
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2022-02-12 00:21:52 +0100

    add ingredient

--- app/src/Presentation/UI/Api/Web/AdminAddIngredientController.php
+++ app/src/Presentation/UI/Api/Web/AdminAddIngredientController.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Presentation\UI\Api\Web;
 
-use App\Core\Component\UserManagement\Application\Write\Command\CreateUserCommand;
+use App\Core\Component\CookingBook\Application\Write\Command\CreateIngredientCommand\CreateIngredientCommand;
 use App\Core\Port\Transport\CommandBus\ISyncCommandBus;
 use App\Core\SharedKernel\Exception\AppRuntimeException;
 use Assert\AssertionFailedException;
@@ -12,9 +12,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class RegisterController extends AbstractController
+class AdminAddIngredientController extends AbstractController
 {
-    #[Route(path: '/auth/register', name: 'register', methods: ['GET', 'POST'])]
+    #[Route(path: '/admin/addingredient', name: 'addingredient', methods: ['GET', 'POST'])]
     public function index(
         ISyncCommandBus $commandBus,
         Request $request
@@ -24,23 +24,26 @@ class RegisterController extends AbstractController
         if ($request->getMethod() === 'POST') {
             try {
                 $body = $request->request->all();
-                $commandBus->dispatch(new CreateUserCommand(
-                    $body['username'],
-                    $body['email'],
-                    $body['password']
+                $commandBus->dispatch(new CreateIngredientCommand(
+                    $body['ingredient']
                 ));
 
-                return $this->redirectToRoute('login');
+                return $this->render('admin/add-ingredient.html.twig', [
+                    'success' => true,
+                    'error' => $error
+                ]);
             } catch(AppRuntimeException | AssertionFailedException $e) {
                 $error = $e->getMessage();
-                return $this->render('auth/register.html.twig', [
-                    'error' => $error
+                return $this->render('admin/add-ingredient.html.twig', [
+                    'error' => $error,
+                    'success' => false
                 ]);
             }
         }
 
-        return $this->render('auth/register.html.twig', [
-            'error' => $error
+        return $this->render('admin/add-ingredient.html.twig', [
+            'error' => $error,
+            'success' => false
         ]);
     }
 }
